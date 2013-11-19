b69bb6d731d4ba6ef8caa0c8a1cbef2afcb101cf
arserbin3 <arserbin3@gmail.com>
2013-11-19 01:07:58 -0500

    moved usings inside namespace

--- FantasyTrack/FantasyTrack/App_Start/BundleConfig.cs
+++ FantasyTrack/FantasyTrack/App_Start/BundleConfig.cs
@@ -1,11 +1,10 @@
-﻿using System.Web;
-using System.Web.Optimization;
-
-namespace FantasyTrack
+﻿namespace FantasyTrack
 {
+    using System.Web;
+    using System.Web.Optimization;
+
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
